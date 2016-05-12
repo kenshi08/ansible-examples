8d0c84f81eb36432428b2e6f4e90717be8ca0b65
Ryan Kelly <rkelly@vmware.com>
2016-05-12 15:25:10 -0700

    Update vragugent.sh

--- vragugent.sh
+++ vragugent.sh
@@ -1,1 +1,10 @@
-
+export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/root/bin
+export Git_URL=$1
+export Playbook=$2
+yum -y install epel-release
+yum -y install ansible git
+cd /tmp
+echo "localhost ansible_connection=local" >> /etc/ansible/hosts
+git clone $Git_URL
+cd ansible-examples/$Playbook/
+ansible-playbook site.yml
